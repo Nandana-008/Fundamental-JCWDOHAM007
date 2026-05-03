cc8f785ae94336597424b8293ffda9483d753f19
Nandana-008 <nandanakrnwn@gmail.com>
2026-05-03 21:23:50 +0700

    add new file

---
+++ Exercise/intro.ts
@@ -0,0 +1,5 @@
+const name: string = "John";
+const age: number = 25;
+const language: string = "Typescript";
+
+console.log(`Hi, my name is ${name}. I'm ${age} years old. I'm learning ${language}.`);
