9ec8c33e5d4cc2d8cb42faca443fee135a442d66
Nandana-008 <nandanakrnwn@gmail.com>
2026-05-03 21:35:03 +0700

    feat: Japanese version

--- Exercise/intro.ts
+++ Exercise/intro.ts
@@ -2,4 +2,4 @@ const name: string = "John";
 const age: number = 25;
 const language: string = "Typescript";
 
-console.log(`Hi, my name is ${name}. I'm ${age} years old. I'm learning ${language}.`);
+console.log(`こんにちは ${name}と申します。${age}　歳です。 今は ${language}について勉強しています。`);
